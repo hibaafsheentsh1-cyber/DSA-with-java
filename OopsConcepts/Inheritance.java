344cbd01e6fc65c74f7153e10aa43913139d1b5c
hiba <hibaafsheentsh1@gmail.com>
2026-06-12 17:28:36 +0530

    Single Inheritance

--- OopsConcepts/Inheritance.java
+++ OopsConcepts/Inheritance.java
@@ -3,17 +3,17 @@
         System.out.println("Eating....");
     }
  }
-     class Dog extends Inheritance{
-        public void bark(){
-            System.out.println("Barking.....");
-        }
+class Dog extends Inheritance{
+    public void bark(){
+        System.out.println("Barking.....");
     }
-        class singleInheritance{
-            public static void main(String[] args) {
-                Dog d = new Dog();
-                d.eat();
-                d.bark();
-            }
-        }
+}
+class singleInheritance{
+    public static void main(String[] args) {
+        Dog d = new Dog();
+        d.eat();
+        d.bark();
+    }
+}
     
 
