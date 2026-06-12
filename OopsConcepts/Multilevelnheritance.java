c0d8f9c75f991a9a6a81d20e35ae256c54e9937a
hiba <hibaafsheentsh1@gmail.com>
2026-06-12 17:35:19 +0530

    Inheritance

---
+++ OopsConcepts/Multilevelnheritance.java
@@ -0,0 +1,23 @@
+class MultiLevel {
+    public void eat(){
+        System.out.println("Eating....");
+    }
+ }
+class Dog extends Inheritance{
+    public void bark(){
+        System.out.println("Barking.....");
+    }
+}
+class Babydog extends Dog{
+    public void weep(){
+        System.out.println("Weeping ......");
+    }
+}
+public class Multilevelnheritance{
+    public static void main(String[] args) {
+        Babydog b = new Babydog();
+        b.bark();
+        b.eat();
+        b.weep();
+    }
+}
