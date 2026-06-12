6c119e4371885f8e6cb9bd19719169ce5d007575
hiba <hibaafsheentsh1@gmail.com>
2026-06-12 17:26:52 +0530

    Initial commit

---
+++ OopsConcepts/Inheritance.java
@@ -0,0 +1,19 @@
+ class Inheritance {
+    public void eat(){
+        System.out.println("Eating....");
+    }
+ }
+     class Dog extends Inheritance{
+        public void bark(){
+            System.out.println("Barking.....");
+        }
+    }
+        class singleInheritance{
+            public static void main(String[] args) {
+                Dog d = new Dog();
+                d.eat();
+                d.bark();
+            }
+        }
+    
+
